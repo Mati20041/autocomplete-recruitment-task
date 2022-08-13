f9a381e522e638d3659ff9356d12805bd67c7ee4
mati20041 <mat20041+github@gmail.com>
2022-08-13 13:17:13 +0200

    fixed debounced, fixed loading indicator, added important tests, stabilization, filtering

--- src/components/autocomplete/Autocomplete.tsx
+++ src/components/autocomplete/Autocomplete.tsx
@@ -1,53 +1,69 @@
-import React, {ChangeEventHandler, useCallback, useMemo, useRef, useState} from "react";
-import {debounce} from "../../debounce";
+import React, {ChangeEventHandler, useCallback, useEffect, useMemo, useState} from "react";
 import "./Autocomplete.css";
 
 export interface AutocompleteProps {
     suggestionProducer: (part: string) => Promise<string[]>,
-    onChange: (value: string) => any
+    onChange: (value: string) => unknown;
+    debounceTime?: number
 }
 
-export const Autocomplete = ({suggestionProducer, onChange}: AutocompleteProps) => {
-    const [suggestions, setSuggestions] = useState<string[]>([]);
-    const [suggestionPhrase, setSuggestionPhrase] = useState("");
-    const inputRef = useRef<HTMLInputElement>(null);
+const filterSuggestions = (value: string, suggestions: string[]) => suggestions.filter(s => s.includes(value));
 
-    // line below is because of the debounce function
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const fetchSuggestions = useCallback(debounce((value: string) => {
-        suggestionProducer(value)
-            .then((suggestions) => {
-                setSuggestions(suggestions);
-                setSuggestionPhrase(value)
-            })
-            .catch(error => {
-                console.log(error); // Probably a better error handling would be preferable
-                setSuggestions([]);
-            });
-    }), [suggestionProducer, setSuggestions, setSuggestionPhrase]);
+export const Autocomplete = ({suggestionProducer, onChange, debounceTime}: AutocompleteProps) => {
+    const [currentValue, setCurrentValue] = useState<string>("");
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [fetchingSuggestions, setFetchingSuggestions] = useState(false);
+
+    useEffect(() => {
+        let canceled = false;
+        let timeoutId: number | null = null;
+        if(currentValue) {
+            setFetchingSuggestions(true);
+            timeoutId = Number(setTimeout(() => {
+                suggestionProducer(currentValue)
+                    .then((suggestions) => {
+                        const filteredSuggestions = filterSuggestions(currentValue, suggestions);
+                        if(!canceled) setSuggestions(filteredSuggestions);
+                    }).catch(error => {
+                        console.log(error); // Probably a better error handling would be preferable
+                        if(!canceled) setSuggestions([]);
+                    }).finally(() => {
+                    if(!canceled) setFetchingSuggestions(false);
+                });
+            }, debounceTime ?? 300))
+        } else {
+            setSuggestions([]);
+            setFetchingSuggestions(false);
+        }
+
+        return () => {
+            canceled = true;
+            if(timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        }
+    }, [currentValue, debounceTime, suggestionProducer])
+
+    const setValue = useCallback((value: string) => {
+        onChange(value);
+        setCurrentValue(value);
+    }, [onChange])
 
     const handleInput: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
         const value = e.target.value;
-        if(value) {
-            // Requirements are not specific if only chosen values from suggestion should be valid or any typed value
-            onChange(value);
-            fetchSuggestions(value);
-        } else {
-            setSuggestions([]);
-        }
-    }, [fetchSuggestions, onChange])
+        setValue(value);
+    }, [setValue])
 
     // We could optimize rendering Autocomplete further by using a reference to an 'observable'
     // and pass it to <Suggestions/> instead of using "useState", but in my opinion this is too hacky
     return <div className="autocomplete">
-        <input type="text" ref={inputRef} onChange={handleInput} placeholder="Please type here for hints"/>
-        <Suggestions onSelect={(value) => {
-            if (inputRef.current) {
-                inputRef.current.value = value;
-                onChange(value);
-                setSuggestions([]);
-            }
-        }} suggestions={suggestions} suggestionPhrase={suggestionPhrase}/>
+        <input value={currentValue} type="text" onChange={handleInput} placeholder="Please type here for hints"/>
+        <div className="autocomplete-suggestions" data-testid="suggestions">
+            {fetchingSuggestions ? "Loading..." :
+                <Suggestions onSelect={(value) => {
+                    setValue(value);
+                }} suggestions={suggestions} suggestionPhrase={currentValue}/>}
+        </div>
     </div>
 }
 
@@ -58,16 +74,21 @@ interface SuggestionProps {
     onSelect: (value: string) => any
 }
 
-const Suggestions = ({suggestions, onSelect, suggestionPhrase}: SuggestionProps) => {
-    if(suggestions.length === 0) {
+const Suggestions = React.memo(({suggestions, onSelect, suggestionPhrase}: SuggestionProps) => {
+    if (suggestions.length === 0) {
         return <></>;
     }
-    return <div className="autocomplete-suggestions" data-testid="suggestions">
-        {suggestions.map(suggestion => <MemoizedItem key={suggestion} suggestionPhrase={suggestionPhrase} onSelect={onSelect} suggestion={suggestion}/>)}
-    </div>;
-};
-const Item = ({suggestion, suggestionPhrase, onSelect}: { suggestion: string, suggestionPhrase:string, onSelect: (value: string) => void }) => {
+    return <>
+        {suggestions.map(suggestion => <Item key={suggestion} suggestionPhrase={suggestionPhrase} onSelect={onSelect}
+                                             suggestion={suggestion}/>)}
+    </>;
+});
 
+const Item = React.memo(({
+                             suggestion,
+                             suggestionPhrase,
+                             onSelect
+                         }: { suggestion: string, suggestionPhrase: string, onSelect: (value: string) => void }) => {
     const highlightedSuggestion = useMemo(() => {
         const startPhrase = suggestion.indexOf(suggestionPhrase);
         const length = suggestionPhrase.length;
@@ -77,11 +98,9 @@ const Item = ({suggestion, suggestionPhrase, onSelect}: { suggestion: string, su
             <mark>{suggestion.slice(startPhrase, endPhrase)}</mark>
             {suggestion.slice(endPhrase, suggestion.length)}
         </>);
-    },[suggestion, suggestionPhrase])
+    }, [suggestion, suggestionPhrase])
 
     const handleClick = useCallback(() => onSelect(suggestion), [onSelect, suggestion])
-    // eslint-disable-next-line jsx-a11y/role-has-required-aria-props
-    return <div role="option" className="autocomplete-item" onClick={handleClick}>{highlightedSuggestion}</div>;
-};
-
-const MemoizedItem = React.memo(Item);
+    return <div role="option" aria-selected="false" className="autocomplete-item"
+                onClick={handleClick}>{highlightedSuggestion}</div>;
+});
--- src/components/autocomplete/__tests__/Autocomplete.spec.tsx
+++ src/components/autocomplete/__tests__/Autocomplete.spec.tsx
@@ -1,12 +1,13 @@
-import {waitFor, render, screen, within} from "@testing-library/react";
+import {render, screen, waitFor, within} from "@testing-library/react";
 import {Autocomplete} from "../Autocomplete";
 import userEvent from "@testing-library/user-event";
-import {wait} from "@testing-library/user-event/dist/utils";
 
 
 const onChange = jest.fn();
 const producer = jest.fn();
 
+
+const suggestionsFake = ["aa", "ab", "bb", "bc"];
 
 describe("Autocomplete", () => {
 
@@ -16,69 +17,108 @@ describe("Autocomplete", () => {
 
     it("should render ok", () => {
         // Arrange & Act
-        const {getInput, querySuggestions} = renderAutocomplete();
+        const {getInput, getSuggestions, queryItems} = renderAutocomplete();
 
         // Assert
         const input = getInput();
         expect(input).toBeInTheDocument();
         expect(input).toHaveAttribute("placeholder", "Please type here for hints");
-        expect(querySuggestions()).not.toBeInTheDocument();
+        expect(getSuggestions()).toBeInTheDocument();
+        expect(getSuggestions()).toBeEmptyDOMElement();
+        expect(queryItems()).toHaveLength(0);
     });
 
-    it("should display suggestions", async () => {
+    it("should should hide suggestions if input value is empty", async () => {
         // Arrange
-        const suggestions = ["a", "aa", "abc"];
-        producer.mockResolvedValue(suggestions)
+        producer.mockResolvedValue(suggestionsFake);
+
+        const {getInput, queryItems} = renderAutocomplete();
+
+        const input = getInput();
+        userEvent.type(input, "a")
+
+        await waitFor(() => expect(queryItems()).toHaveLength(2));
+        const items = queryItems();
+
+        expect(items[1].textContent).toEqual(suggestionsFake[1]);
+
+        // Act
+        userEvent.click(items[1]);
+
+        // Assert
+        await waitFor(() => expect(queryItems()).toHaveLength(0));
+        expect(input).toHaveValue(suggestionsFake[1]);
+        expect(onChange).toHaveBeenCalledWith("a");
+        expect(onChange).toHaveBeenCalledWith(suggestionsFake[1]);
+    });
+
+    it("should hide suggestions and set input value on choosing", async () => {
+        // Arrange
+        producer.mockResolvedValue(suggestionsFake);
+
         // Act
         const {getInput, getSuggestions} = renderAutocomplete();
 
         const input = getInput();
         userEvent.type(input, "a")
 
-        await waitFor(() => expect(getSuggestions()).toBeInTheDocument());
-
-        const items = within(getSuggestions()).getAllByRole("option")
-        expect(items.map(it => it.textContent)).toEqual(suggestions);
-    });
-
-    it("should should hide suggestions if input value is empty", async () => {
-        // Arrange
-        const suggestions = ["a", "aa", "abc"];
-        producer.mockResolvedValue(suggestions)
-        // Act
-        const {getInput, getSuggestions, querySuggestions} = renderAutocomplete();
-
-        const input = getInput();
-        userEvent.type(input, "a")
-
-        await waitFor(() => expect(getSuggestions()).toBeInTheDocument());
-        const items = within(getSuggestions()).getAllByRole("option");
-
-        expect(items[1].textContent).toEqual(suggestions[1]);
-        userEvent.click(items[1]);
-
-        await waitFor(() => expect(querySuggestions()).not.toBeInTheDocument());
-        expect(input).toHaveValue(suggestions[1])
-    });
-
-    it("should hide suggestions and set input value on choosing", async () => {
-        // Arrange
-        const suggestions = ["a", "aa", "abc"];
-        producer.mockResolvedValue(suggestions)
-        // Act
-        const {getInput, getSuggestions, querySuggestions} = renderAutocomplete();
-
-        const input = getInput();
-        userEvent.type(input, "a")
-
-        await waitFor(() => expect(getSuggestions()).toBeInTheDocument());
+        // Assert
+        await waitFor(() => expect(getSuggestions()).not.toBeEmptyDOMElement());
 
         userEvent.clear(input);
-        await waitFor(() => expect(querySuggestions()).not.toBeInTheDocument());
+        await waitFor(() => expect(getSuggestions()).toBeEmptyDOMElement());
     });
 
-    // I should add a test for text highlight, and also verify `onChange` handler, but I run out of time ;)
+    it("should display filtered suggestions based on input", async () => {
+        // Arrange
+        producer.mockResolvedValue(suggestionsFake)
+        // Act
+        const {getInput, getItems} = renderAutocomplete();
 
+        const input = getInput();
+        userEvent.type(input, "a");
+
+        // Assert
+        await waitFor(() => expect(getItems()).toHaveLength(2));
+
+        expect(getItems().map(it => it.textContent)).toEqual(["aa", "ab"]);
+    });
+
+    it("should show loading indicator while fetching", async () => {
+        // Arrange
+        producer.mockReturnValue(new Promise(() => {}))
+        // Act
+        const {getInput, getSuggestions, queryItems} = renderAutocomplete();
+
+        const input = getInput();
+        userEvent.type(input, "a")
+
+        // Assert
+        await waitFor(() => expect(getSuggestions()).toBeInTheDocument());
+
+        expect(getSuggestions()).toHaveTextContent("Loading...");
+        expect(queryItems()).toHaveLength(0);
+    });
+
+    // I should add a test for text highlight
+
+
+    it("should highlight found text", async () => {
+        // Arrange
+        producer.mockResolvedValue(["would"])
+        // Act
+        const {getInput, getItems} = renderAutocomplete();
+
+        const input = getInput();
+        userEvent.type(input, "oul");
+
+        // Assert
+        await waitFor(() => expect(getItems()).toHaveLength(1));
+
+        const items = getItems();
+
+        expect(items[0]).toContainHTML("w<mark>oul</mark>d");
+    });
 })
 
 
@@ -87,7 +127,8 @@ const renderAutocomplete = () => {
 
     return {
         getInput: () => screen.getByRole("textbox"),
-        querySuggestions: () => screen.queryByTestId("suggestions"),
-        getSuggestions: () => screen.getByTestId("suggestions")
+        getSuggestions: () => screen.getByTestId("suggestions"),
+        getItems: () => within(screen.getByTestId("suggestions")).getAllByRole("option"),
+        queryItems: () => within(screen.getByTestId("suggestions")).queryAllByRole("option")
     }
 }
